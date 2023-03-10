44062b358826ef6778362b174956d74a016f383c
Danil <shvydkodan@gmail.com>
2023-03-10 23:39:11 +0200

    All UI parts added

--- app/src/main/java/com/example/instagram/MyProfile.kt
+++ app/src/main/java/com/example/instagram/MyProfile.kt
@@ -2,10 +2,17 @@ package com.example.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MyProfile : AppCompatActivity() {
+
+    private var circleImageView: CircleImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+
+        circleImageView = findViewById(R.id.imageProfile)
+
+
     }
 }
